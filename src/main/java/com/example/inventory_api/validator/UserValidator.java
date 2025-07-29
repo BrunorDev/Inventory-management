35e5975970c3b13c5c4147d49c87ee06fd2924d4
Bruno Reis <bruno.ms.p@outlook.com>
2025-07-29 19:16:10 -0300

    feat: new validation methods have been added

--- src/main/java/com/example/inventory_api/validator/UserValidator.java
+++ src/main/java/com/example/inventory_api/validator/UserValidator.java
@@ -1,9 +1,11 @@
 package com.example.inventory_api.validator;
 
+import com.example.inventory_api.domain.entities.User;
 import com.example.inventory_api.dtos.userDTO.UserRequest;
 import com.example.inventory_api.exceptions.BusinessException;
 import com.example.inventory_api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class UserValidator {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     public static final String PASSWORD_REGEX =
             "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
@@ -26,5 +29,30 @@ public class UserValidator {
             throw new BusinessException("E-mail already in use");
         }
         validatePassword(request.password());
+    }
+    public void validateEmailUniqueness(String currentEmail, String newEmail) {
+        if (!currentEmail.equals(newEmail) && repository.existsByEmail(newEmail)) {
+            throw new BusinessException("E-mail already in use");
+        }
+    }
+
+    public void validatePasswordChange(String newPassword, String currentPasswordHash) {
+        validatePassword(newPassword);
+
+        if (passwordEncoder.matches(newPassword, currentPasswordHash)) {
+            throw new BusinessException("The new password must differ from current one");
+        }
+    }
+
+    public void ensureUserIsActive(User user) {
+        if (!user.isActive()) {
+            throw new BusinessException("User is deactivated");
+        }
+    }
+
+    public void ensureUserIsInactive(User user) {
+        if (user.isActive()) {
+            throw new BusinessException("User is already active");
+        }
     }
 }
