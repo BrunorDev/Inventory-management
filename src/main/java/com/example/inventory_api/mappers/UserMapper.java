7f6fd448da9ba6191de4826d8b8903c43025237a
Bruno Reis <bruno.ms.p@outlook.com>
2025-07-29 18:01:22 -0300

    feat: mapping between entity and dto added

---
+++ src/main/java/com/example/inventory_api/mappers/UserMapper.java
@@ -0,0 +1,22 @@
+package com.example.inventory_api.mappers;
+
+import com.example.inventory_api.domain.entities.User;
+import com.example.inventory_api.dtos.userDTO.UserRequest;
+import com.example.inventory_api.dtos.userDTO.UserResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "stockMovements", ignore = true)
+    @Mapping(target = "passwordHash", source = "password")
+    @Mapping(target = "id", ignore = true)
+    User toUser(UserRequest request);
+
+    UserResponse toUserResponse(User user);
+
+    List<UserResponse> toUserResponseList(List<User> users);
+}
