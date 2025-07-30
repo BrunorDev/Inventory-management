7540f1fd735a4bc07a1e24f978fbfcefe0633798
Bruno Reis <bruno.ms.p@outlook.com>
2025-07-30 15:51:11 -0300

    feat: implemented 'findByEmail' method and updated 'save' method

--- src/main/java/com/example/inventory_api/services/UserService.java
+++ src/main/java/com/example/inventory_api/services/UserService.java
@@ -1,6 +1,5 @@
 package com.example.inventory_api.services;
 
-import com.example.inventory_api.domain.entities.User;
 import com.example.inventory_api.dtos.loginDTO.LoginRequest;
 import com.example.inventory_api.dtos.userDTO.UserRequest;
 import com.example.inventory_api.dtos.userDTO.UserResponse;
@@ -16,13 +15,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static com.example.inventory_api.validator.UserValidator.validatePassword;
-
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
     private final UserMapper mapper;
     private final UserValidator validator;
     private final PasswordEncoder passwordEncoder;
@@ -33,23 +30,31 @@ public class UserService {
         var user = mapper.toUser(request);
         user.setProfile(request.profile());
         user.setActive(true);
-        return mapper.toUserResponse(repository.save(user));
+        user.setPasswordHash(passwordEncoder.encode(request.password()));
+        return mapper.toUserResponse(userRepository.save(user));
     }
 
     public List<UserResponse> getAll() {
-        var users = repository.findAllByActiveTrue();
+        var users = userRepository.findAllByActiveTrue();
         return mapper.toUserResponseList(users);
     }
 
     public UserResponse getById(Long id) {
-        var user = repository.findById(id)
+        var user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
         return mapper.toUserResponse(user);
     }
 
+    public UserResponse getByEmail(String email){
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User account not found"));
+
+        return mapper.toUserResponse(user);
+    }
+
     public UserResponse login(LoginRequest request) {
-        var user = repository.findByEmail(request.email())
+        var user = userRepository.findByEmail(request.email())
                 .orElseThrow(() -> new ResourceNotFoundException("User account not found"));
 
         if (!user.isActive()) {
@@ -65,7 +70,7 @@ public class UserService {
 
     @Transactional
     public UserResponse update(Long id, UserRequest request) {
-        var user = repository.findByIdAndActiveTrue(id)
+        var user = userRepository.findByIdAndActiveTrue(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
         validator.validateEmailUniqueness(user.getEmail(), request.email());
@@ -74,22 +79,22 @@ public class UserService {
         user.setName(request.name());
         user.setEmail(request.email());
         user.setPasswordHash(passwordEncoder.encode(request.password()));
-        return mapper.toUserResponse(repository.save(user));
+        return mapper.toUserResponse(userRepository.save(user));
     }
 
     @Transactional
     public void updatePassword(Long id, String newPassword) {
-        var user = repository.findByIdAndActiveTrue(id)
+        var user = userRepository.findByIdAndActiveTrue(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
         validator.validatePasswordChange(newPassword, user.getPasswordHash());
         user.setPasswordHash(passwordEncoder.encode(newPassword));
-        repository.save(user);
+        userRepository.save(user);
     }
 
     @Transactional
     public void activateUser(Long id) {
-        var user = repository.findById(id)
+        var user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
         validator.ensureUserIsInactive(user);
@@ -98,7 +103,7 @@ public class UserService {
 
     @Transactional
     public void deactivateUser(Long id) {
-        var user = repository.findByIdAndActiveTrue(id)
+        var user = userRepository.findByIdAndActiveTrue(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
         user.setActive(false);
