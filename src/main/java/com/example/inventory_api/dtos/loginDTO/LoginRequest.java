b595fa733a711c4b2ba936693e13ea44a0776e31
Bruno Reis <bruno.ms.p@outlook.com>
2025-07-31 18:31:20 -0300

    feat: validation annotations have been added

--- src/main/java/com/example/inventory_api/dtos/loginDTO/LoginRequest.java
+++ src/main/java/com/example/inventory_api/dtos/loginDTO/LoginRequest.java
@@ -1,7 +1,14 @@
 package com.example.inventory_api.dtos.loginDTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
         String password
 ) {
 }
