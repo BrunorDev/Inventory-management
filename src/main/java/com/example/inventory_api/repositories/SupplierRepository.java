22cfc23e9b93d15f717fb168e0e0a0e8c576347e
Bruno Reis <bruno.ms.p@outlook.com>
2025-07-31 18:02:41 -0300

    feat: repository has been added

---
+++ src/main/java/com/example/inventory_api/repositories/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.example.inventory_api.repositories;
+
+import com.example.inventory_api.domain.entities.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+}
