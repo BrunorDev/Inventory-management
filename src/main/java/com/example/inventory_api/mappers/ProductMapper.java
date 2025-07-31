c77ae4a3d74f57fb23b10cfff4a3a3b15ea98fec
Bruno Reis <bruno.ms.p@outlook.com>
2025-07-31 19:14:43 -0300

    feat: mapping between entity and dto added

---
+++ src/main/java/com/example/inventory_api/mappers/ProductMapper.java
@@ -0,0 +1,23 @@
+package com.example.inventory_api.mappers;
+
+import com.example.inventory_api.domain.entities.Product;
+import com.example.inventory_api.dtos.productDTO.ProductRequest;
+import com.example.inventory_api.dtos.productDTO.ProductResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mapping(target = "supplier", ignore = true)
+    @Mapping(target = "stockMovements", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    Product toProduct(ProductRequest request);
+
+    ProductResponse toProductResponse(Product product);
+
+    List<ProductResponse> toProductResponseList(List<Product> products);
+}
