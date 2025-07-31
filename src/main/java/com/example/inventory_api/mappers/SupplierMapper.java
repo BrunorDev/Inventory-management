cb3995241402b85d3b2bff520859ed0ac8a705a8
Bruno Reis <bruno.ms.p@outlook.com>
2025-07-31 17:57:16 -0300

    feat: mapping methods between entity and dto have been added

---
+++ src/main/java/com/example/inventory_api/mappers/SupplierMapper.java
@@ -0,0 +1,21 @@
+package com.example.inventory_api.mappers;
+
+import com.example.inventory_api.domain.entities.Supplier;
+import com.example.inventory_api.dtos.supplierDTO.SupplierRequest;
+import com.example.inventory_api.dtos.supplierDTO.SupplierResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SupplierMapper {
+
+    @Mapping(target = "products", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    Supplier toSupplier(SupplierRequest request);
+
+    SupplierResponse toSupplierResponse(Supplier supplier);
+
+    List<SupplierResponse> toSupplierResponseList(List<Supplier> suppliers);
+}
