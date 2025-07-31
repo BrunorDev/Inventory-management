bfb3397aa9f37a5991b268e456ed6b6dfdbd12a9
Bruno Reis <bruno.ms.p@outlook.com>
2025-07-31 17:51:01 -0300

    feat: validation annotations were added

--- src/main/java/com/example/inventory_api/dtos/userDTO/UserRequest.java
+++ src/main/java/com/example/inventory_api/dtos/userDTO/UserRequest.java
@@ -1,11 +1,22 @@
 package com.example.inventory_api.dtos.userDTO;
 
 import com.example.inventory_api.domain.enums.Profile;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UserRequest(
+        @NotBlank
         String name,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
         String password,
+
+        @NotNull
         Profile profile
 ) {
 }
