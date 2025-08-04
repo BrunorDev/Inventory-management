abb7df30f23db97f7f9a9ec300262f4d69af4696
Bruno Reis <bruno.ms.p@outlook.com>
2025-08-04 16:15:00 -0300

    refactor: save method has been completely refactored

--- src/main/java/com/example/inventory_api/services/ProductService.java
+++ src/main/java/com/example/inventory_api/services/ProductService.java
@@ -9,10 +9,12 @@ import com.example.inventory_api.mappers.ProductMapper;
 import com.example.inventory_api.repositories.CategoryRepository;
 import com.example.inventory_api.repositories.ProductRepository;
 import com.example.inventory_api.repositories.SupplierRepository;
+import com.example.inventory_api.validator.ProductValidator;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -22,17 +24,12 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final SupplierRepository supplierRepository;
     private final CategoryRepository categoryRepository;
+    private final ProductValidator validator;
     private final ProductMapper mapper;
 
     @Transactional
     public ProductResponse save(ProductRequest request, Long supplierId, Long categoryId) {
-        if (request.quantityStock() < 0) {
-            throw new BusinessException("Quantity cannot be negative");
-        }
-
-        if (productRepository.existsBySku(request.sku())) {
-            throw new BusinessException("SKU is already in use, try again with a new code");
-        }
+        validator.validateSkuCode(request);
 
         var supplier = supplierRepository.findById(supplierId)
                 .orElseThrow(() -> new ResourceNotFoundException("Supplier not found"));
@@ -67,8 +64,34 @@ public class ProductService {
         if (product.getQuantityStock() > 0) {
             throw new BusinessException("Product still has quantities available in stock");
         }
-
         product.setAvailable(false);
+    }
+
+    @Transactional
+    public ProductResponse update(Long productId, ProductRequest request) {
+        var product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+
+        validator.validateBeforeUpdate(request, productId);
+
+        product.setName(request.name());
+        product.setDescription(request.description());
+        product.setSku(request.sku());
+        product.setQuantityStock(request.quantityStock());
+        product.setExpiryDate(request.expiryDate());
+        product.setPurchasePrice(request.purchasePrice());
+        product.setPriceOnSale(request.priceOnSale());
+
+        var category = categoryRepository.findById(request.categoryId())
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+
+        var supplier = supplierRepository.findById(request.supplierId())
+                .orElseThrow(() -> new ResourceNotFoundException("Supplier not found"));
+
+        product.setCategory(category);
+        product.setSupplier(supplier);
+
+        return mapper.toProductResponse(product);
     }
 
 //    Methods QUERIES / FILTERS
