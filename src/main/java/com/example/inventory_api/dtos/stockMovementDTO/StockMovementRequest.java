f073b328b09cd8800b609ccda4703e8206b3acde
Bruno Reis <bruno.ms.p@outlook.com>
2025-08-05 13:26:37 -0300

    feat: validation annotations have been added

--- src/main/java/com/example/inventory_api/dtos/stockMovementDTO/StockMovementRequest.java
+++ src/main/java/com/example/inventory_api/dtos/stockMovementDTO/StockMovementRequest.java
@@ -1,12 +1,25 @@
 package com.example.inventory_api.dtos.stockMovementDTO;
 
 import com.example.inventory_api.domain.enums.Type;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record StockMovementRequest(
+        @NotNull
         Long productId,
+
+        @NotNull
         Long userId,
+
+        @NotNull
         Type type,
+
+        @NotNull
+        @Positive
+        @Min(1)
         Integer quantity,
+
         String note
 ) {
 }
