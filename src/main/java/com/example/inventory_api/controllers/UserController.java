dffdff564e4436a69230f333429f4be36741d872
Bruno Reis <bruno.ms.p@outlook.com>
2025-08-05 14:25:02 -0300

    feat: UserController has been implemented

---
+++ src/main/java/com/example/inventory_api/controllers/UserController.java
@@ -0,0 +1,78 @@
+package com.example.inventory_api.controllers;
+
+import com.example.inventory_api.dtos.loginDTO.LoginRequest;
+import com.example.inventory_api.dtos.userDTO.UserRequest;
+import com.example.inventory_api.dtos.userDTO.UserResponse;
+import com.example.inventory_api.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> save(@RequestBody @Valid UserRequest request) {
+        var user = userService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponse> login(@RequestBody @Valid LoginRequest request) {
+        return ResponseEntity.ok(userService.login(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(
+            @PathVariable Long id,
+            @RequestBody @Valid UserRequest request) {
+        return ResponseEntity.ok(userService.update(id, request));
+    }
+
+    @PatchMapping("/{id}/password")
+    public ResponseEntity<Void> updatePassword(
+            @PathVariable Long id,
+            @RequestBody @Valid String newPassword) {
+        userService.updatePassword(id, newPassword);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/activate")
+    public ResponseEntity<Void> activateUser(@PathVariable Long id) {
+        userService.activateUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/deactivate")
+    public ResponseEntity<Void> deactivateUser(@PathVariable Long id) {
+        userService.deactivateUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAll() {
+        var users = userService.getAll();
+        return users.isEmpty()
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<UserResponse> getByEmail(@RequestParam String email) {
+        return ResponseEntity.ok(userService.getByEmail(email));
+    }
+}
+
