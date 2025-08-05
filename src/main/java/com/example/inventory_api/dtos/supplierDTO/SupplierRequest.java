48c22f45909f07ec1ceec968e5e240dcecacece5
Bruno Reis <bruno.ms.p@outlook.com>
2025-08-05 18:35:35 -0300

    fix: unnecessary annotation was removed

--- src/main/java/com/example/inventory_api/dtos/supplierDTO/SupplierRequest.java
+++ src/main/java/com/example/inventory_api/dtos/supplierDTO/SupplierRequest.java
@@ -1,14 +1,12 @@
 package com.example.inventory_api.dtos.supplierDTO;
 
 import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.br.CNPJ;
 
 public record SupplierRequest(
         @NotBlank
         String name,
 
         @NotBlank
-        @CNPJ
         String cnpj,
 
         @NotBlank
