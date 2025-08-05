81fb7161056d8638e8b184796964f3d0957786ca
Bruno Reis <bruno.ms.p@outlook.com>
2025-08-05 18:31:01 -0300

    feat: forgotten field has been added

--- src/main/java/com/example/inventory_api/dtos/userDTO/UserResponse.java
+++ src/main/java/com/example/inventory_api/dtos/userDTO/UserResponse.java
@@ -3,6 +3,7 @@ package com.example.inventory_api.dtos.userDTO;
 import com.example.inventory_api.domain.enums.Profile;
 
 public record UserResponse(
+        Long id,
         String name,
         String email,
         Profile profile
