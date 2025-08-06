3efcab2e9234abd1dc04852559d389e71f19b8e7
Bruno Reis <bruno.ms.p@outlook.com>
2025-08-06 17:19:25 -0300

    feat: unnecessary dto has been removed

--- src/main/java/com/example/inventory_api/dtos/categoryDTO/CategorySummary.java
+++
@@ -1,6 +0,0 @@
-package com.example.inventory_api.dtos.categoryDTO;
-
-public record CategorySummary(
-        String name
-) {
-}
--- src/main/java/com/example/inventory_api/dtos/supplierDTO/SupplierSummary.java
+++
@@ -1,8 +0,0 @@
-package com.example.inventory_api.dtos.supplierDTO;
-
-public record SupplierSummary(
-        String name,
-        String cnpj,
-        String contact
-) {
-}
