2ffe2f2362a0e8f4709f9069da7406acb74627df
Bruno Reis <bruno.ms.p@outlook.com>
2025-08-06 17:14:26 -0300

    refactor: all query endpoints have been updated

--- src/main/java/com/example/inventory_api/controllers/ProductController.java
+++ src/main/java/com/example/inventory_api/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.example.inventory_api.controllers;
 import com.example.inventory_api.domain.entities.Product;
 import com.example.inventory_api.dtos.productDTO.ProductRequest;
 import com.example.inventory_api.dtos.productDTO.ProductResponse;
+import com.example.inventory_api.dtos.productDTO.ProductSummary;
 import com.example.inventory_api.services.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +29,7 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductResponse>> getAll() {
+    public ResponseEntity<List<ProductSummary>> getAll() {
         var products = productService.getAll();
         return products.isEmpty()
                 ? ResponseEntity.noContent().build()
@@ -47,22 +48,22 @@ public class ProductController {
     }
 
     @GetMapping("/filter/name")
-    public ResponseEntity<List<ProductResponse>> getByName(@RequestParam String name) {
+    public ResponseEntity<List<ProductSummary>> getByName(@RequestParam String name) {
         return ResponseEntity.ok(productService.getByName(name));
     }
 
     @GetMapping("/filter/quantity")
-    public ResponseEntity<List<ProductResponse>> getAllByQuantity(@RequestParam Integer quantity) {
+    public ResponseEntity<List<ProductSummary>> getAllByQuantity(@RequestParam Integer quantity) {
         return ResponseEntity.ok(productService.getAllByQuantity(quantity));
     }
 
     @GetMapping("/filter/category")
-    public ResponseEntity<List<ProductResponse>> getAllByCategory(@RequestParam Long categoryId) {
+    public ResponseEntity<List<ProductSummary>> getAllByCategory(@RequestParam Long categoryId) {
         return ResponseEntity.ok(productService.getAllByCategory(categoryId));
     }
 
     @GetMapping("/filter/supplier")
-    public ResponseEntity<List<ProductResponse>> getAllBySupplier(@RequestParam Long supplierId) {
+    public ResponseEntity<List<ProductSummary>> getAllBySupplier(@RequestParam Long supplierId) {
         return ResponseEntity.ok(productService.getAllBySupplier(supplierId));
     }
 
